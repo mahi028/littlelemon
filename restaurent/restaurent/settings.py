282751c5ea54b1f51bd9b4659cd9bab9932a5dc3
mahi028 <tewarimohit028@gmail.com>
2025-05-03 04:39:16 +0530

    bug

--- restaurent/restaurent/settings.py
+++ restaurent/restaurent/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'restaurent.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
